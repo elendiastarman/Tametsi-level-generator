93da009803cdd631117949c088e99c9b6eb04334
El'endia Starman <lee314b@gmail.com>
2018-06-22 23:11:37 -0400

    Added random demo.

--- TametsiGenerator.py
+++ TametsiGenerator.py
@@ -1,4 +1,5 @@
 import sys
+import random
 
 from TametsiSolver import Puzzle, uncompress
 
@@ -43,5 +44,34 @@ def CL_demo(num):
   print("Score:", score)
 
 
+def collapse_difficulty_steps(difficulty_steps, multiplier=2):
+  return sum([multiplier ** x * y for x, y in enumerate(difficulty_steps)])
+
+
+def random_demo(width, height):
+  max_score = 0
+
+  while 1:
+    compressed = ''.join([random.choice('.*') for x in range(width * height)])
+    print("compressed:", compressed)
+    board, revealed, constraints = uncompress(width, height, compressed)
+
+    puzzle = Puzzle(board, revealed, constraints)
+    solved = puzzle.solve()
+    print("Solved?", solved)
+
+    score = score_puzzle(puzzle)
+    print("Score:", score)
+
+    if solved:
+      collapsed = collapse_difficulty_steps(score, multiplier=100)
+      if collapsed > max_score:
+        max_score = collapsed
+        print("Best so far: {} with collapsed score {}".format(compressed, collapsed))
+
+    print()
+
+
 if __name__ == '__main__':
-  CL_demo(int(sys.argv[1]) if len(sys.argv) > 1 else 1)
+  # CL_demo(int(sys.argv[1]) if len(sys.argv) > 1 else 1)
+  random_demo(6, 6)
