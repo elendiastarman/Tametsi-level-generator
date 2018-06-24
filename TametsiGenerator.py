3895f56aaceacad1382a0d9efeace7fa9e24f46d
El'endia Starman <lee314b@gmail.com>
2018-06-23 20:53:05 -0400

    Yeah, should've made more commits...but here's a ton of work.

--- TametsiGenerator.py
+++ TametsiGenerator.py
@@ -4,7 +4,7 @@ import random
 from TametsiSolver import Puzzle, uncompress
 
 
-def score_puzzle(puzzle):
+def extract_difficulty_steps(puzzle):
   difficulty_steps = [0]
   step = 0
 
@@ -40,38 +40,101 @@ def CL_demo(num):
   solved = puzzle.solve()
   print("Solved?", solved)
 
-  score = score_puzzle(puzzle)
-  print("Score:", score)
+  difficulty_steps = get_difficulty_steps(width, height, compressed)
+  print("Score:", difficulty_steps)
 
 
-def collapse_difficulty_steps(difficulty_steps, multiplier=2):
-  return sum([multiplier ** x * y for x, y in enumerate(difficulty_steps)])
+def get_difficulty_steps(width, height, compressed):
+  board, revealed, constraints = uncompress(width, height, compressed)
+  puzzle = Puzzle(board, revealed, constraints)
+  puzzle.solve()
+
+  return extract_difficulty_steps(puzzle)
+
+
+def raw_difficulty(width, height, compressed, multiplier=100):
+  difficulty_steps = get_difficulty_steps(width, height, compressed)
+
+  if difficulty_steps[-1] > 0:
+    return sum([multiplier ** x * y for x, y in enumerate(difficulty_steps)])
+
+  else:
+    return 0
+
+
+def smooth_difficulty(width, height, compressed):
+  difficulty_steps = get_difficulty_steps(width, height, compressed)
+
+  if difficulty_steps[-1] == 0:
+    return 0
+
+  if len(difficulty_steps) < 2:
+    return difficulty_steps[0] ** 0.5
+
+  score = 0
+
+  for index in range(len(difficulty_steps) - 1):
+    a, b = difficulty_steps[index:index + 2]
+    x, y = min(a, b), max(a, b)
+
+    score += x + x / (y - x + 1)
+
+  return score
 
 
 def random_demo(width, height):
   max_score = 0
 
   while 1:
-    compressed = ''.join([random.choice('.*') for x in range(width * height)])
-    print("compressed:", compressed)
-    board, revealed, constraints = uncompress(width, height, compressed)
+    compressed = ''.join([random.choice('..*') for x in range(width * height)])
+    collapsed = raw_difficulty(width, height, compressed)
 
-    puzzle = Puzzle(board, revealed, constraints)
-    solved = puzzle.solve()
-    print("Solved?", solved)
+    if collapsed > max_score:
+      max_score = collapsed
+      print("Best so far: {} with collapsed score {}\n".format(compressed, collapsed))
 
-    score = score_puzzle(puzzle)
-    print("Score:", score)
 
-    if solved:
-      collapsed = collapse_difficulty_steps(score, multiplier=100)
-      if collapsed > max_score:
-        max_score = collapsed
-        print("Best so far: {} with collapsed score {}".format(compressed, collapsed))
+def evolutionary_demo(width, height, seeds=10):
+  board_strings = {}
+  choices = '..*'
+  metric = smooth_difficulty
 
-    print()
+  for _ in range(seeds):
+    compressed = ''.join([random.choice(choices) for x in range(width * height)])
+    board_strings[compressed] = metric(width, height, compressed)
+
+  best = list(board_strings.keys())[:]
+
+  round_num = 0
+  while 1:
+    round_num += 1
+    added = False
+
+    for index, bstr1 in enumerate(best):
+      for bstr2 in best[index:]:
+        x = random.randint(1, width * height - 1)
+        nstr = bstr1[:x] + bstr2[x:]
+
+        # mutate
+        for i in range(len(nstr)):
+          if random.random() < 1 / 2:
+            nstr = nstr[:i] + random.choice(choices) + nstr[i + 1:]
+
+        if nstr not in board_strings:
+          board_strings[nstr] = metric(width, height, nstr)
+          added = True
+
+    if not added:
+      continue
+
+    best = sorted(board_strings.keys(), key=lambda x: board_strings[x])[-seeds:]
+
+    print("Best of round {}:".format(round_num))
+    for bstr in best:
+      print("String {} had score {}.".format(bstr, board_strings[bstr]))
 
 
 if __name__ == '__main__':
   # CL_demo(int(sys.argv[1]) if len(sys.argv) > 1 else 1)
-  random_demo(6, 6)
+  # random_demo(6, 6)
+  evolutionary_demo(6, 6, int(sys.argv[1]) if len(sys.argv) > 1 else 10)
---
+++ TametsiWriter.py
@@ -0,0 +1,89 @@
+import os
+import sys
+import datetime
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+env = Environment(
+  loader=FileSystemLoader('.'),
+  autoescape=select_autoescape(['xml']),
+)
+
+level_template = env.get_template('leveltemplate.xml')
+
+# print(template.render(title="test"))
+
+
+def write_level(width, height, compressed, **parameters):
+  params = dict(
+    title='test',
+    tile_size=10,
+  )
+  params.update(parameters)
+
+  tile_size = params['tile_size']
+  points = '-{s},-{s},{s},-{s},{s},{s},-{s},{s}'.format(s=tile_size / 2)
+
+  nodes = []
+  columns = []
+
+  for i in range(width * height):
+    neighbors = []
+    nodes.append(dict(
+      id=i,
+      neighbors=neighbors,
+      position=((i % width) * tile_size, (i // width) * tile_size),
+      has_mine=compressed[i] == '*',
+      secret=compressed[i] == '?',
+      points=points,
+    ))
+
+    for dx in [-1, 0, 1]:
+      if i % width + dx < 0 or i % width + dx >= width:
+        continue
+
+      for dy in [-1, 0, 1]:
+        if i // width + dy < 0 or i // width + dy >= height:
+          continue
+
+        if [dx, dy] == [0, 0]:
+          continue
+
+        neighbors.append(i + dx + width * dy)
+
+  # vertical column hints
+  for j in range(width):
+    columns.append(dict(
+      ids=[j + k * width for k in range(height)],
+      text_location=(j * tile_size, -tile_size),
+    ))
+
+  # horizontal column hints
+  for j in range(height):
+    columns.append(dict(
+      ids=list(range(j * width, j * width + width)),
+      text_location=(-tile_size, j * tile_size),
+    ))
+
+  params['nodes'] = nodes
+  params['columns'] = columns
+  params['puzzle_id'] = len(os.listdir('puzzles')) + 1
+
+  level = level_template.render(**params)
+
+  name = 'puzzles/{}_{}.puz'.format(datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d'), params['title'])
+  with open(name, 'w') as file:
+    file.write(level)
+
+
+if __name__ == '__main__':
+  if len(sys.argv) < 4:
+    width, height = 6, 6
+    compressed = '.*.?...*.?..*.***?**.?..*?*.*....*.?'  # CL 1
+  else:
+    width = int(sys.argv[1])
+    height = int(sys.argv[2])
+    compressed = sys.argv[3]
+
+  title = 'test_' + compressed.replace('.', '0').replace('*', '1').replace('?', '2')
+
+  write_level(width, height, compressed, title=title)
