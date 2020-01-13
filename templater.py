6b88b32b8eea0fd98fdd079a0d8a50f6f67ee053
El'endia Starman <lee314b@gmail.com>
2020-01-13 00:04:57 -0500

    New level type.

--- templater.py
+++ templater.py
@@ -268,7 +268,128 @@ def holey_render(compressed, size):
   title = f'Holey {size}x{size} with score {scored}'
   tile_text = 'HOL'
 
-  return title, tile_text, nodes, columns, scored
+  return title, tile_text, nodes, columns, None, scored
+
+
+def L_shape_grid(compressed, size, depth):
+  """
+    33
+    13
+  2114
+  2244
+  """
+  size, depth = int(size), int(depth)
+  side_length = 2 * size * 2 ** depth
+  points = '-{s},-{s},{s},-{s},{s},{s},-{s},{s}'.format(s=0.96 / 2)
+  cindex = 0
+
+  board = []
+  revealed = []
+  constraints = []
+  nodes = []
+  columns = []
+  colors = [
+    dict(ids=[], color='RED', is_dark=False),
+    dict(ids=[], color='ORANGE', is_dark=False),
+    dict(ids=[], color='GREEN', is_dark=False),
+    dict(ids=[], color='BLUE', is_dark=False),
+  ]
+
+  id_map = dict()
+  for y in range(side_length):
+    for x in range(side_length):
+      if x < side_length // 2 - 1 and y < side_length // 2 - 1:
+        continue
+      else:
+        id_map[(x, y)] = len(id_map)
+
+  pos_to_id = lambda x, y: id_map.get((x, y))
+
+  for y in range(side_length):
+    for x in range(side_length):
+      if x < side_length // 2 - 1 and y < side_length // 2 - 1:
+        continue
+
+      cell_id = pos_to_id(x, y)
+      neighbors = []
+
+      for dy in range(-1, 2):
+        for dx in range(-1, 2):
+          if dx == dy == 0:
+            continue
+
+          neighbor_id = pos_to_id(x + dx, y + dy)
+          if neighbor_id:
+            neighbors.append(neighbor_id)
+
+      if x < side_length // 2 and y < side_length // 2:
+        what = '.'
+        revealed.append(cell_id)
+      else:
+        what = compressed[cindex] if compressed else ''
+        cindex += 1
+
+        temp_x, temp_y, threshold = x, y, side_length // 4
+
+        for i in range(depth):
+          if threshold <= temp_x < 3 * threshold and threshold <= temp_y < 3 * threshold:
+            if i == depth - 1:
+              colors[0]['ids'].append(cell_id)
+            else:
+              temp_x -= threshold
+              temp_y -= threshold
+
+          elif temp_x < threshold * 2:
+            if i == depth - 1:
+              colors[1]['ids'].append(cell_id)
+            else:
+              temp_x, temp_y = temp_y - threshold * 2, threshold * 2 - temp_x - 1
+
+          elif temp_y < threshold * 2:
+            if i == depth - 1:
+              colors[2]['ids'].append(cell_id)
+            else:
+              temp_x, temp_y = threshold * 2 - temp_y - 1, temp_x - threshold * 2
+
+          else:
+            if i == depth - 1:
+              colors[3]['ids'].append(cell_id)
+            else:
+              temp_x -= threshold * 2
+              temp_y -= threshold * 2
+
+          threshold //= 2
+
+      board.append([cell_id, what, neighbors])
+
+      if compressed:
+        nodes.append(dict(
+          id=cell_id,
+          neighbors=neighbors,
+          position=(x, y),
+          has_mine=what == '*',
+          secret=what == '?',
+          revealed=cell_id in revealed,
+          points=points,
+        ))
+
+  constraints.append([sum([c[1] == '*' for c in board]), [c[0] for c in board if c[0] not in revealed]])
+
+  for color in colors:
+    constraints.append([sum(board[n][1] == '*' for n in color['ids']), color['ids']])
+
+  num = 3 * side_length ** 2 // 4
+  board.sort(key=lambda c: c[0] in revealed)  # praise be to Python's stable sort
+
+  if not compressed:
+    return num, board, revealed, constraints, None
+  else:
+    result = Puzzle(board, revealed, constraints).solve()
+    scored = score(result, 'seqnum')
+    title = f'L-shape {size}-{depth} with score {scored}'
+    tile_text = 'L'
+
+    return title, tile_text, nodes, columns, colors, scored
 
 
 def make_template(method, *args, **kwargs):
@@ -276,6 +397,7 @@ def make_template(method, *args, **kwargs):
     combination_lock=combination_lock,
     cl_corner_bite=cl_corner_bite,
     holey=holey,
+    l_shape_grid=partial(L_shape_grid, None),
   )
 
   return methods[method](*args, **kwargs)
@@ -286,6 +408,7 @@ def render_template(method, compressed, *args, **kwargs):
     combination_lock=combination_lock_render,
     cl_corner_bite=cl_corner_bite_render,
     holey=holey_render,
+    l_shape_grid=L_shape_grid,
   )
 
   return methods[method](compressed, *args, **kwargs)
