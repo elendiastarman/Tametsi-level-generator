fee6879a35ba9e397fe88c9810ad3863215bac39
El'endia Bleem Starman <elendiastarman@users.noreply.github.com>
2021-09-30 01:13:20 -0400

    Filled out README and added comments in some places (#2)
    Full README, moar comments, and improved usability.

--- tester.py
+++ tester.py
@@ -6,19 +6,23 @@ from scorer import score
 
 
 def run(level_id=None, verbose=False):
-  filenames = []
-
-  with open('test/index') as index:
-    for line in index.read().split('\n'):
-      id, name = line.split(' ')
-      filenames.append(name)
-
-      if level_id and id == level_id:
-        filenames = [name]
-        break
-
+  # a shorthand to make it easy to test the latest puzzle you generated
   if level_id == '-1':
     filenames = ['../latest.puz']
+
+  else:
+    filenames = []
+    # if you've just cloned the repo, this test/index file does not exist!
+    # this file solely consists of lines in the form "[id] [filename]"
+    # where filenames are in that same folder
+    with open('test/index') as index:
+      for line in index.read().split('\n'):
+        id, name = line.strip().split(' ')
+        filenames.append(name)
+
+        if level_id and id == level_id:
+          filenames = [name]
+          break
 
   print('filenames:', filenames)
   for index, filename in enumerate(filenames):
@@ -43,6 +47,16 @@ def run(level_id=None, verbose=False):
     scored = score(result, 'seqnum')
     print(f'{index + 1:3} {filename:20}: {et - st:.3f} seconds, solved {result["solved"]}, score {scored:.3f} - {name}')
 
+
+# python test.py [puzzle_id [--verbose]]
+# with no args, this runs the scorer on all puzzles defined in /test/index
+# with a puzzle id, this runs the scorer on that one (defined in /test/index)
+# --verbose enables a ton of debug print statements
+
+# NOTE THAT YOU NEED TO CREATE /test and put puzzle files in there
+# AS WELL AS /test/index
+# which is formatted like:
+#   puzzle_id puzzle_filename
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
--- writer.py
+++ writer.py
@@ -1,3 +1,4 @@
+import re
 import sys
 import time
 import datetime
@@ -5,39 +6,49 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 from templater import render_template
 
+# Jinja2 setup stuff
 env = Environment(
   loader=FileSystemLoader('.'),
   autoescape=select_autoescape(['xml']),
 )
 
 level_template = env.get_template('leveltemplate.xml')
+# ^^^ this is a jinja2 renderer that loads a file and later, when .render is called, fills out the file with the given variables
+# this is a generic file that captures the overall structure of a Tametsi puzzle file
 
 
 def write_level(board_template, compressed, *template_args, **parameters):
+  # default but overwritable attributes
   params = dict(
-    title='test',
     tile_size=10,
   )
   params.update(parameters)
 
+  # render_template takes a template (actually the lookup name) that then gets instantiated
+  # according to the compressed string (and any extra args like size) and returns its attributes
+  # this does solve the ensuing puzzle in order to give its score
   title, tile_text, nodes, columns, colors, score = render_template(board_template, compressed, *template_args)
 
-  params['nodes'] = nodes
-  params['columns'] = columns
-  params['colors'] = colors
-  params['puzzle_id'] = int(time.time())
-  params['title'] = title
-  params['tile_text'] = tile_text
-  params['score'] = score
+  params['nodes'] = nodes  # describes the cells and their relations with each other
+  params['columns'] = columns  # groups some cells into some number of column hints (in any direction, including horizontal and diagonal) [technically *any* set of cells]
+  params['colors'] = colors  # groups some cells into some number of color hints
+  params['puzzle_id'] = int(time.time())  # there's probably a better way to do this but ids need to be unique, otherwise Tametsi gets confused
+  params['title'] = title  # this is displayed in-game while solving the puzzle
+  params['tile_text'] = tile_text  # this is displayed in-game while choosing a puzzle
+  params['score'] = score  # the computed score of the puzzle
 
+  # Jinja-render the template file with this info
   level = level_template.render(**params)
 
+  # vvv This just makes a unique filename
   today = datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d')
   filename_safe_title = params['title'].replace(' ', '-')
 
   name = f'puzzles/{today}_{filename_safe_title}.puz'
   print(name)
+  # ^^^
 
+  # Write to the filename and also the latest file, for immediate testing
   with open(name, 'w') as file:
     file.write(level)
 
@@ -45,8 +56,14 @@ def write_level(board_template, compressed, *template_args, **parameters):
     file.write(level)
 
 
+# python writer.py <template_name> <compressed> [arg1] [arg2] ...
+# <compressed> is a string of `.*?` that represent the cells as empty (.), mined (*), or unknown (?)
+
 if __name__ == '__main__':
   # write_level('combination_lock', compressed, size)
   # write_level('cl_corner_bite', compressed, size)
   # write_level('holey', compressed, size)
-  write_level('l_shape_grid', *sys.argv[1:])
+  # write_level('l_shape_grid', compressed, size, depth)
+  args = [int(arg) if re.match(r'\d+', arg) else arg for arg in sys.argv[1:]]
+
+  write_level(*args)  # just pipes in the command line args
